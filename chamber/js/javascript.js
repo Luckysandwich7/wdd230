07a35ba93e6f1baaf3d2df9d8de9c82f392eef77
Luckysandwich7 <Ryanalvord@hotmail.com>
2022-05-13 18:25:58 -0600

    chamber files initial upload

---
+++ chamber/js/javascript.js
@@ -0,0 +1,44 @@
+/* copyright date
+date last edited*/
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date(document.lastModified);
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+const fullDateMod = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+// using getElementById
+document.querySelector("#dateMod").textContent = fullDateMod;
+// ****************************
+
+document.querySelector('#copyDate').textContent = year;
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn")
+x.onclick = toggleMenu;
