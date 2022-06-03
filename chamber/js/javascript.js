cf21e54c13506a5dbbf2e886190cf4effa9f8b16
Luckysandwich7 <Ryanalvord@hotmail.com>
2022-06-02 22:18:13 -0600

    Week 6 assignment Upload

--- chamber/js/javascript.js
+++ chamber/js/javascript.js
@@ -49,5 +49,19 @@ const currentdate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).form
 
 datefield.innerHTML = `${currentdate}`;
 
-var date = new Date();
-$("#banner" + date.getDay()).show();
+/* Banner dispaly on Monday and Tuesday*/
+var weekday = new Array(7);
+weekday [0] = 'Sunday';
+weekday [1] = 'Monday';
+weekday [2] = 'Tuesday';
+weekday [3] = 'Wednesday';
+weekday [4] = 'Thursday';
+weekday [5] = 'Friday';
+weekday [6] = 'Saturday';
+
+var currentDate = new Date();
+weekdayValue = currentDate.getDay();
+
+if (weekdayValue > 0 <= 3) {
+	document.getElementById("banner").innerHTML = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'
+}
