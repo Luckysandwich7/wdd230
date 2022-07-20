f8cc5818b0939bd18cb1b9d50f0ac5b4e03c1c00
Luckysandwich7 <Ryanalvord@hotmail.com>
2022-07-19 19:43:02 -0600

    Temple page edits and css for all pages

---
+++ templeinnandsuite/stylesheets/temple_page_list.js
@@ -0,0 +1,54 @@
+const requestURL = 'https://luckysandwich7.github.io/wdd230/templeinnandsuite/temple.json';
+const cards = document.querySelector('.cards');
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+
+    const affiliates = jsonObject['affiliates'];
+    affiliates.forEach(displayAffiliates);
+  });
+
+function displayAffiliates(affiliate) {
+  // Create elements to add to the document
+  let card = document.createElement('section');
+  let h2 = document.createElement('h2');
+  let phone = document.createElement('p');
+  let email = document.createElement('p');
+  let address = document.createElement('p');
+  let image = document.createElement('img');
+  let ordinance = document.createElement('p');
+  let session = document.createElement('p');
+  let closed = document.createElement('p');
+
+  // Change the textContent property of the h2 element to contain the prophet's full name
+  h2.textContent = `${affiliate.name}`;
+  phone.textContent = `Phone: ${affiliate.phone}`;
+  email.textContent = `Email: ${affiliate.email}`;
+  address.textContent = `Address: ${affiliate.address}`;
+  ordinance.textContent = `Ordinances: ${affiliate.ordinance}`;
+  session.textContent = `Schedule Session: ${affiliate.session}`;
+  closed.textContent =  `Closed: ${affiliate.closed}`
+
+  // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values.
+  image.setAttribute('src', affiliate.image);
+  image.setAttribute('alt', `${affiliate.name}`);
+  image.setAttribute('loading', 'lazy');
+
+  // Add/append the section(card) with the h2 element
+  card.classList.add('card')
+  card.appendChild(h2);
+  card.appendChild(image);
+  card.appendChild(phone);
+  card.appendChild(email);
+  card.appendChild(address);
+  card.appendChild(ordinance);
+  card.appendChild(session);
+  card.appendChild(closed);
+
+  // Add/append the existing HTML div with the cards class with the section(card)
+  document.querySelector('div.cards').appendChild(card);
+}
